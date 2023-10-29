65bfd30ff1853a6434d582c89f9114f3d2e3badb
souravrjpt <souravrajput2760@gmail.com>
2023-10-29 17:02:01 +0530

    added code

--- Documents/myProjects/growingpaws/growingpaws/src/body/sections/section1.js
+++ Documents/myProjects/growingpaws/growingpaws/src/body/sections/section1.js
@@ -1,138 +1,55 @@
-import React from "react"
-import { i1 } from "../images";
-// import { Section1 } from "../../cssfiles";
-import { Section1 } from "../../cssfiles/section1.css";
+import React from 'react';
+import { i1 } from '../images';
+
+import { Section1 } from '../../cssfiles/section1.css';
 
 export default function SectionOne() {
-    return (
-        <>
-            <section class="wrapper">
-                <div class="container">
-                    
-                    <div class="row">
-                        <div class="col-sm-12 col-md-6 col-lg-4 mb-4">
-                            <div class="card text-dark card-has-bg click-col" >
-                            <img class="card-img d-none" src={i1} />
-                            <div class="card-img-overlay d-flex flex-column">
-                                <div class="card-body">
-                                    <small class="card-meta mb-2">Thought Leadership</small>
-                                    <h4 class="card-title mt-0 "><a class="text-dark" herf="https://creativemanner.com">Web Developmet Lorem Ipsum Sit Amet Consectetur dipisi?</a></h4>
-                                    <small><i class="far fa-clock"></i> October 15, 2020</small>
-                                </div>
-                                {/* <div class="card-footer">
-                                    <div class="media">
-                                        <img class="mr-3 rounded-circle" src="https://assets.codepen.io/460692/internal/avatars/users/default.png?format=auto&version=1688931977&width=80&height=80" alt="Generic placeholder image" style="max-width:50px" />
-                                        <div class="media-body">
-                                            <h6 class="my-0 text-dark d-block">Oz Coruhlu</h6>
-                                            <small>Director of UI/UX</small>
-                                        </div>
-                                    </div>
-                                </div> */}
-                            </div>
-                        </div>
-                        </div>
-                        <div class="col-sm-12 col-md-6 col-lg-4 mb-4"><div class="card text-dark card-has-bg click-col" >
-                            <img class="card-img d-none" src={i1} alt="Creative Manner Design Lorem Ipsum Sit Amet Consectetur dipisi?" />
-                            <div class="card-img-overlay d-flex flex-column">
-                                <div class="card-body">
-                                    <small class="card-meta mb-2">Thought Leadership</small>
-                                    <h4 class="card-title mt-0 "><a class="text-dark" herf="https://creativemanner.com">Creative Manner Lorem Ipsum Sit Amet Consectetur dipisi?</a></h4>
-                                    <small><i class="far fa-clock"></i> October 15, 2020</small>
-                                </div>
-                                {/* <div class="card-footer">
-                                    <div class="media">
-                                        <img class="mr-3 rounded-circle" src="https://assets.codepen.io/460692/internal/avatars/users/default.png?format=auto&version=1688931977&width=80&height=80" alt="Generic placeholder image" style="max-width:50px" />
-                                        <div class="media-body">
-                                            <h6 class="my-0 text-dark d-block">Oz Coruhlu</h6>
-                                            <small>Director of UI/UX</small>
-                                        </div>
-                                    </div>
-                                </div> */}
-                            </div>
-                        </div></div>
-                        <div class="col-sm-12 col-md-6 col-lg-4 mb-4"><div class="card text-dark card-has-bg click-col" >
-                            <img class="card-img d-none" src={i1} alt="Creative Manner Design Lorem Ipsum Sit Amet Consectetur dipisi?" />
-                            <div class="card-img-overlay d-flex flex-column">
-                                <div class="card-body">
-                                    <small class="card-meta mb-2">Thought Leadership</small>
-                                    <h4 class="card-title mt-0 "><a class="text-dark" herf="https://creativemanner.com">Design Studio Lorem Ipsum Sit Amet Consectetur dipisi?</a></h4>
-                                    <small><i class="far fa-clock"></i> October 15, 2020</small>
-                                </div>
-                                {/* <div class="card-footer">
-                                    <div class="media">
-                                        <img class="mr-3 rounded-circle" src="https://assets.codepen.io/460692/internal/avatars/users/default.png?format=auto&version=1688931977&width=80&height=80" alt="Generic placeholder image" style="max-width:50px" />
-                                        <div class="media-body">
-                                            <h6 class="my-0 text-dark d-block">Oz Coruhlu</h6>
-                                            <small>Director of UI/UX</small>
-                                        </div>
-                                    </div>
-                                </div> */}
-                            </div>
-                        </div></div>
+  return (
+    <>
+      <section class='wrapper' style={{height:'10rem'}}>
+        <div class='container' >
+          <div class='row' >
+            
+            <div class='col-sm-12 col-md-4 col-lg-4' >
+              <div class='card text-dark card-has-bg click-col '>
+                <img id='first' class='card-img d-none' />
+                <div id='card1' class='card-img-overlay d-flex flex-column'>
+                  <div class='card-body'>
+                    <h2 class='card-title mt-0'>RENT A PET</h2>
+                  </div>
+                </div>
+              </div>
+            </div>
 
-                        <div class="col-sm-12 col-md-6 col-lg-4 mb-4"><div class="card text-dark card-has-bg click-col" >
-                            <img class="card-img d-none" src={i1} alt=" Lorem Ipsum Sit Amet Consectetur dipisi?" />
-                            <div class="card-img-overlay d-flex flex-column">
-                                <div class="card-body">
-                                    <small class="card-meta mb-2">Thought Leadership</small>
-                                    <h4 class="card-title mt-0 "><a class="text-dark" herf="https://creativemanner.com">UI/UX Design Lorem Ipsum Sit Amet Consectetur dipisi?</a></h4>
-                                    <small><i class="far fa-clock"></i> October 15, 2020</small>
-                                </div>
-                                {/* <div class="card-footer">
-                                    <div class="media">
-                                        <img class="mr-3 rounded-circle" src="https://assets.codepen.io/460692/internal/avatars/users/default.png?format=auto&version=1688931977&width=80&height=80" alt="Generic placeholder image" style="max-width:50px" />
-                                        <div class="media-body">
-                                            <h6 class="my-0 text-dark d-block">Oz Coruhlu</h6>
-                                            <small>Director of UI/UX</small>
-                                        </div>
-                                    </div>
-                                </div> */}
-                            </div>
-                        </div></div>
-                        <div class="col-sm-12 col-md-6 col-lg-4 mb-4"><div class="card text-dark card-has-bg click-col" >
-                            <img class="card-img d-none" src={i1} alt="Creative Manner Design Lorem Ipsum Sit Amet Consectetur dipisi?" />
-                            <div class="card-img-overlay d-flex flex-column">
-                                <div class="card-body">
-                                    <small class="card-meta mb-2">Thought Leadership</small>
-                                    <h4 class="card-title mt-0 "><a class="text-dark" herf="https://creativemanner.com">Creative Manner Design Lorem Ipsum Sit Amet Consectetur dipisi?</a></h4>
-                                    <small><i class="far fa-clock"></i> October 15, 2020</small>
-                                </div>
-                                {/* <div class="card-footer">
-                                    <div class="media">
-                                        <img class="mr-3 rounded-circle" src="https://assets.codepen.io/460692/internal/avatars/users/default.png?format=auto&version=1688931977&width=80&height=80" alt="Generic placeholder image" style="max-width:50px" />
-                                        <div class="media-body">
-                                            <h6 class="my-0 text-dark d-block">Oz Coruhlu</h6>
-                                            <small>Director of UI/UX</small>
-                                        </div>
-                                    </div>
-                                </div> */}
-                            </div>
-                        </div></div>
-                        <div class="col-sm-12 col-md-6 col-lg-4 mb-4"><div class="card text-dark card-has-bg click-col" >
-                            <img class="card-img d-none" src={i1} alt="Creative Manner Design Lorem Ipsum Sit Amet Consectetur dipisi?" />
-                            <div class="card-img-overlay d-flex flex-column">
-                                <div class="card-body">
-                                    <small class="card-meta mb-2">Thought Leadership</small>
-                                    <h4 class="card-title mt-0 "><a class="text-dark" herf="https://creativemanner.com">Creative Manner Design Lorem Ipsum Sit Amet Consectetur dipisi?</a></h4>
-                                    <small><i class="far fa-clock"></i> October 15, 2020</small>
-                                </div>
-                                {/* <div class="card-footer">
-                                    <div class="media">
-                                        <img class="mr-3 rounded-circle" src="https://assets.codepen.io/460692/internal/avatars/users/default.png?format=auto&version=1688931977&width=80&height=80" alt="Generic placeholder image" style="max-width:50px" />
-                                        <div class="media-body">
-                                            <h6 class="my-0 text-dark d-block">Oz Coruhlu</h6>
-                                            <small>Director of UI/UX</small>
-                                        </div>
-                                    </div>
-                                </div> */}
-                            </div>
-                        </div></div>
-
-                    </div>
-
-                </div >
-            </section >
-
-        </>
-    );
+            <div class='col-sm-12 col-md-6 col-lg-4 mb-4'>
+              <div class='card text-dark card-has-bg click-col'>
+                <img
+                  class='card-img d-none'
+                  alt='Creative Manner Design Lorem Ipsum Sit Amet Consectetur dipisi?'
+                />
+                <div id='card2' class='card-img-overlay d-flex flex-column'>
+                  <div class='card-body'>
+                    <h2 class='card-title mt-0'>RENT A PET</h2>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class='col-sm-12 col-md-6 col-lg-4 mb-4'>
+              <div class='card text-dark card-has-bg click-col'>
+                <img
+                  class='card-img d-none'
+                  alt='Creative Manner Design Lorem Ipsum Sit Amet Consectetur dipisi?'
+                />
+                <div id='card3' class='card-img-overlay d-flex flex-column'>
+                  <div class='card-body'>
+                    <h2 class='card-title mt-0'>RENT A PET</h2>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
